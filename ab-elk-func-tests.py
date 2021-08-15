7f52166f44edf2bd723867504ed489e4a8ee03f8
Yogesh Kumar Balasubramanian <yogeshkumarbalu@gmail.com>
2021-08-15 20:30:27 +0200

    Update ab-elk-func-tests.py

--- ab-elk-func-tests.py
+++ ab-elk-func-tests.py
@@ -23,7 +23,7 @@ def make_functional_tests_elasticsearch():
 			# print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
 	
 	except Exception as e:
-		print("updated")
+		print("STOP dont mess the PROD environment")
 		#email and escalate and stop CD in the pipeline
 		#print (e)
 
