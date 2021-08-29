ec43e306353cb786bd7e120b603cd37252e9ab5b
Yogesh Kumar Balasubramanian <yogeshkumarbalu@gmail.com>
2021-08-29 15:06:45 +0200

    Create ab-func-tests.py

---
+++ ab-func-tests.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
+
+
+def feed_index():
+    try:
+        doc = {
+            'author': 'yogesh',
+            'text': 'Am i being fed into index - SUCCESS',
+            'timestamp': datetime.now(),
+        }
+        res = es.index(index="integration-test-index", id=2, body=doc)
+        print(res['result'])
+    
+    except Exception as e:
+        print("notify developer - mail or create tickets - etc" + str(e))
+        print("errorELKFunctional")
+
+
+feed_index()
