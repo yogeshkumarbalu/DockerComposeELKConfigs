6770086664ca33c2e16df9ef3018c7f866f18272
Yogesh Kumar Balasubramanian <yogeshkumarbalu@gmail.com>
2021-08-29 14:48:35 +0200

    Update ab-integration-tests.py

--- ab-integration-tests.py
+++ ab-integration-tests.py
@@ -1,13 +1,21 @@
-from datetime import datetime
 from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
-res = es.get(index="test-index", id=1)
-print(res['_source'])
+def is_old_data_available():
+    try:
+        res = es.get(index="test-index", id=1)
+        print(res['_source'])
 
-es.indices.refresh(index="test-index")
+        es.indices.refresh(index="test-index")
 
-res = es.search(index="test-index", body={"query": {"match_all": {}}})
-print("Got %d Hits:" % res['hits']['total']['value'])
-for hit in res['hits']['hits']:
-    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+        res = es.search(index="test-index", body={"query": {"match_all": {}}})
+        print("Got %d Hits:" % res['hits']['total']['value'])
+        for hit in res['hits']['hits']:
+            print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+    
+    except Exception as e:
+        print("notify developer - mail or create tickets - etc" + str(e))
+        print("errorELKIntegration")
+
+
+is_old_data_available()
