10996f0b00227cef43653bac09f489caaf9605d6
Yogesh Kumar Balasubramanian <yogeshkumarbalu@gmail.com>
2021-08-29 15:08:43 +0200

    Update ab-func-tests.py

--- ab-func-tests.py
+++ ab-func-tests.py
@@ -7,11 +7,11 @@ def feed_index():
     try:
         doc = {
             'author': 'yogesh',
-            'text': 'Am i being fed into index - SUCCESS',
+            'text': 'Am i being fed into index',
             'timestamp': datetime.now(),
         }
         res = es.index(index="integration-test-index", id=2, body=doc)
-        print(res['result'])
+        print(res['result'] + "SUCCESS")
     
     except Exception as e:
         print("notify developer - mail or create tickets - etc" + str(e))
